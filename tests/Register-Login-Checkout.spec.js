c0a8860107f72449c9253594a9f00998ed0c102d
Amer Ćivić <amer.civic.22@size.ba>
2026-04-09 16:34:26 +0200

    Trying to fix github actions by taking a screenshot

--- tests/Register-Login-Checkout.spec.js
+++ tests/Register-Login-Checkout.spec.js
@@ -47,16 +47,19 @@ test ('Home-Register-Login-Cart-SignOut ToolShop automation',async({page})=>{
     });
     await test.step('Provide valid data in all required fields and click register',async ()=> {
          await registerPage.register(user);
+         console.log('URL after login:', page.url()); 
+         await page.screenshot({ path: 'after-register.png', fullPage: true });
+
     });
     await test.step('Wait for the user to be redirected to the login page.',async ()=> {
           await expect(page).toHaveURL(/auth\/login/, { timeout: 15000 });
     });
 
-    await expect(loginPage.emailInput).toBeVisible();
-  
+    // await expect(loginPage.emailInput).toBeVisible();
+    
    // Dodao sam screenshot da se napravi 
    /*  await page.screenshot({ path: 'before-login.png', fullPage: true });
-console.log('Current URL before login:', page.url()); */
+*/
     /* await expect(page).toHaveURL('https://practicesoftwaretesting.com/auth/login');
     await page.waitForLoadState('domcontentloaded'); */
 // await page.waitForLoadState('networkidle');
